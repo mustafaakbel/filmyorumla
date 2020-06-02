6df0ae8e85ef92995490e874fde50f94a9465b14
mustafaakbel <mustafaakbel@outlook.com>
2020-06-02 10:28:01 +0300

    load image güncellendi

--- moviesingle_light.php
+++ moviesingle_light.php
@@ -39,7 +39,7 @@
 <body>
 <!--preloading-->
 <div id="preloader">
-    <img class="logo" src="images/logo1.png" alt="" width="119" height="58">
+    <img class="logo" src="images/load.png" alt="" width="119" height="58">
     <div id="status">
         <span></span>
         <span></span>
